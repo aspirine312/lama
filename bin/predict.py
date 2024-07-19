cba0331e2ac5143439c7f327e8a8e54e5521b149
aspirine312 <114738194+aspirine312@users.noreply.github.com>
2024-07-19 15:44:44 +0800

    predict.py

--- bin/predict.py
+++ bin/predict.py
@@ -58,25 +58,7 @@ def main(predict_config: OmegaConf):
         model = load_checkpoint(train_config, checkpoint_path, strict=False, map_location='cpu')
         model.freeze()
 
-        # ------------------ add by myself ------------------ #
-        from fvcore.nn import FlopCountAnalysis
-        
-        def count_parameters(model):
-            return sum(p.numel() for p in model.parameters())
 
-        num_parameter = count_parameters(model)
-        print(f" total parameters = {num_parameter} ")
-
-        def count_flops(model, input_tensor):
-            flops = FlopCountAnalysis(model, input_tensor)
-            return flops.total()
-
-        input_tensor = torch.randn(1, 1, 224, 224)  # 根据模型的输入大小进行调整
-
-        # 计算 FLOPs
-        flops = count_flops(model, input_tensor)
-        print(f" total flops = {flops}")
-        # ------------------ add by myself ------------------ #
         
         if not predict_config.get('refine', False):
             model.to(device)
@@ -114,6 +96,26 @@ def main(predict_config: OmegaConf):
             cur_res = cv2.cvtColor(cur_res, cv2.COLOR_RGB2BGR)
             cv2.imwrite(cur_out_fname, cur_res)
 
+    # ------------------ add by myself ------------------ #
+    from fvcore.nn import FlopCountAnalysis
+    
+    def count_parameters(model):
+        return sum(p.numel() for p in model.parameters())
+
+    num_parameter = count_parameters(model)
+    print(f" total parameters = {num_parameter} ")
+
+    def count_flops(model, input_tensor):
+        flops = FlopCountAnalysis(model, input_tensor)
+        return flops.total()
+
+    input_tensor = torch.randn(1, 1, 224, 224)  # 根据模型的输入大小进行调整
+
+    # 计算 FLOPs
+    flops = count_flops(model, input_tensor)
+    print(f" total flops = {flops}")
+    # ------------------ add by myself ------------------ #
+    
     except KeyboardInterrupt:
         LOGGER.warning('Interrupted by user')
     except Exception as ex:
